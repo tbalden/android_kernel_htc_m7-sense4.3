c24f4171c7a0cd7551ea88563f5d1a902196e271
tbalden <illespal@gmail.com>
2013-09-03 22:53:02 +0200

    gpu/cpu oc/uc/uv tuning from @n3ocort3x aosp/cm10.2 kernels

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -326,13 +326,13 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-		policy->cpuinfo.min_freq = 135000;
-		policy->cpuinfo.max_freq = 1998000;
+		policy->min = 84000;
+   		policy->max = 1990000;
 #endif
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-	policy->min = 135000;
-	policy->max = 1998000;
+        policy->cpuinfo.min_freq = 84000;
+        policy->cpuinfo.max_freq = 1990000;
 #endif
 
 #ifdef CONFIG_ARCH_APQ8064
